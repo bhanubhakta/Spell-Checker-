37326db21b944ea48b994062f276ca8072a3c8fa
bistaprad <pradeep.bistapb@gmail.com>
2013-01-03 15:24:34 +0545

    improved result that previous one

--- spell_checker.rb
+++ spell_checker.rb
@@ -8,7 +8,7 @@ require 'mysql'
 def train features
   model = Hash.new(1)
   features.each_hash{|h| 
-    model[h['name'].downcase] = Math::log((Float(h['freq'])))
+    model[h['name'].downcase] = (Float(h['freq']))
     #puts Float(h['freq'])
   }
 
@@ -40,7 +40,11 @@ end
 
 def known_edits2 word
   result = []
-  edits1(word).each {|e1| edits1(e1).each {|e2| result << e2 if NWORDS.has_key?(e2) }}
+  edits1(word).each {|e1| 
+    edits1(e1).each {|e2| 
+      result << e2 if NWORDS.has_key?(e2) 
+    }
+  }
   result.empty? ? nil : result
 end
 
@@ -50,47 +54,70 @@ def known words
 end
 
 def correct word
-  unless (edits1_candidate = (known(edits1(word)))).nil? 
-    edits1_candidate = (known(edits1(word))).uniq
+
+  unless (e1 = (known(edits1(word)))).nil? 
+    e1 = (known(edits1(word))).uniq
   end
-
-  #edits2_candididate = (known_edits2(word)).uniq
-
-  unless (edits2_candidate = (known_edits2(word))).nil? 
-    edits2_candidate = (known_edits2(word)).uniq
+ 
+  unless (e2 = (known_edits2(word))).nil? 
+    e2 = (known_edits2(word)).uniq
   end
 
   if(word.length<= 3)
-    edits2_candidate = nil
+    e2 = nil
   end
 
-  # puts "Entered Word"
-  # puts word
-  # puts
+   puts "Entered Word"
+   puts word
+   puts
 
   #puts "Edit Distance 1"
   #print_words(edits1_candidate)
-  edits1_candidate =sort_words_by_popularity(edits1_candidate,word)
+  e1 =sort_words_by_popularity(e1,word)
 
   #puts "Edit Distance 2"
   #print_words(edits2_candidate)
-  edits2_candidate = sort_words_by_popularity(edits2_candidate,word)
-
-  unless edits1_candidate.nil?
-    edits1_candidate = score(edits1_candidate, edits1_candidate.length)  
+  e2 = sort_words_by_popularity(e2,word)
+  unless e1.nil?
+  unless e2.nil?
+    edit2 = []
+    e2.each {|w|
+        if !e1.include?(w) 
+          edit2.push(w) 
+         end   
+    }
+    e2 = edit2
+  end
+  end
+  
+  total_freq = 0
+  unless e1.nil?
+    e1.each{|w|
+      total_freq += NWORDS[w]
+    }
+  end
+  unless e2.nil?
+    e2.each{|w|
+    total_freq += NWORDS[w]
+  }
   end
 
-  unless edits2_candidate.nil?
-    edits2_candidate = score(edits2_candidate, edits2_candidate.length)
+  unless e1.nil?
+    e1 = score(e1, word, total_freq)  
   end
 
-  #puts "Top Five Words from ED1 & ED2 based on score \n"
-  select_top_five(edits1_candidate, edits2_candidate) 
+  unless e2.nil?
+    e2 = score(e2, word, total_freq)
+  end
+
+  puts "Top Five Words from ED1 & ED2 based on score \n"
+  select_top_five(e1, e2,word) 
 end
 
 #Finds the top five suggestion from edit distance one and two,
 #accoring to their scores.
-def select_top_five(ed1,ed2)
+def select_top_five(ed1,ed2,word)
+  
   if(ed1 == nil)
     words = ed2 
   elsif(ed2 == nil)
@@ -98,45 +125,66 @@ def select_top_five(ed1,ed2)
   else
     words = ed1.merge(ed2)
   end
-  words.sort_by{|k,v| -v}
-  words  = words.first 5
 
-  words_only = Array.new()
-  words.each{ |k,v|
-    words_only.push(k) 
-  }  
+  words = words.sort_by{|k,v| -v}.first 5
+  words = Hash[*words.flatten]
+  final_list = Hash.new{}
 
-  # words.each { |k,v| 
-  #   print (String(v)+" "+String(NWORDS[k])+"  "+k+"\n")
-  # }
+  if !NWORDS.has_key?(word)
+      words.each_with_index { |(k,v),index|
+        if index == (words.length-1)
+          final_list[word] = v*0.5
+        else
+          final_list[k] = v
+        end
+      }
+      words = final_list
+  end 
   
-  return words_only  #list of words 
-  #return word       #Hash of words and corresponding score 
+  # words_only = Array.new()
+  # words.each{ |k,v|
+  #   words_only.push(k) 
+  # }
+
+  words.each { |k,v| 
+    print (String(v)+" "+String(NWORDS[k])+"  "+k+"\n")
+  }
+  #puts words.class
+  #return words_only  #list of words 
+  return words       #Hash of words and corresponding score 
 end
 
 # Calculates the socre for each correction candidate
-def score(words, len)
+def score(words, word, total_freq)
   hash = Hash.new{}
-  total_freq = 0
+  # total_freq = 0
+  # words.each{|w|
+  #   total_freq += NWORDS[w]
+  # }
+  
   words.each{|w|
-    total_freq += NWORDS[w]
-  }
-  words.each{|w|
-    s = NWORDS[w]/(total_freq*len)
+    # if word == w
+    #   s = 100
+    # else 
+    #   #s = (NWORDS[w]/(total_freq)*100).round(4)  
+    #   s = (NWORDS[w]/(total_freq*words.length)*100).round(4)  
+    #   #puts s
+    # end
+    s = (NWORDS[w]/(total_freq*words.length)*100).round(4)
     hash[w] = s   
   }
-  hash.sort_by{|k,v| -v}
+  hash = hash.sort_by{|k,v| -v}
   hash.each{|k,v| 
   #print (String(v)+" "+String(NWORDS[k])+"  "+k+"\n")
-  }
+  } 
+  hash = Hash[*hash.flatten]
   return hash
 end
 
 def sort_words_by_popularity(words,word)
   unless words.nil?
-    words.delete(word)
+    #words.delete(word)
     sorted_words = words.sort_by{|w| -NWORDS[w] }
-    len = sorted_words
     array = sorted_words.first 10
     #print_words(array)
     return array
@@ -153,3 +201,5 @@ def print_words words
   end
   puts "\n\n"
 end
+
+correct("rose")
