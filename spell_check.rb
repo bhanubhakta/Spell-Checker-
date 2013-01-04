415116a3bdb3b4a5a03daf880e7acaf4e9c49f55
Pradeep Bista <pradeepbista@cloudfactory.(none)>
2013-01-04 17:54:36 +0545

    dictionary with multiple years

--- spell_check.rb
+++ spell_check.rb
@@ -2,14 +2,11 @@ class SpellCheck
 	attr_accessor :year, :ed1_tot_freq, :ed2_tot_freq, :typed_word, :count_ed1, :count_ed2
 	attr_reader :dictionary , :score , :suggestion_list 
   
-  def initialize(name, year)
+  def initialize(name)
     @name = name
-    @year = year
-    @dictionary = {}
+    @dictionary = Hash.new(Hash.new)
     @score = []
     @suggestion_list = []
-    filename = year.to_s + ".txt"
-    
   end
 
   # For the selection of year...
@@ -18,18 +15,52 @@ class SpellCheck
 	# end
 
   # For user 
-	def add_hash_to_dictionary(options)
-    @dictionary = options.merge(@dictionary)
-	end
+	# def add_hash_to_dictionary(options)
+ #    @dictionary = options.merge(@dictionary)
+	# end
 
-  # For adding CSV file to dictionary
-  # def add_file_to_dictionary()
-  # end
+  # For adding file instance to dictionary
+  def add_to_dictionary(file_name)
+
+    year = file_name.split(".")
+    year = year[0][3..year[0].length-1]
+    
+    dict_by_year = add_file_to_dictionary(File.new(file_name).read)
+
+    @dictionary[year] = dict_by_year
+
+    puts @dictionary
+
+  end
+
+  def add_file_to_dictionary(features)
+    dict_by_year = Hash.new{}
+    total_freq = 0
+
+    #count total frequency
+    features.each_line do |line|
+      splits = line.split(",")
+      total_freq += Integer(splits[2])
+    end
+
+    features.each_line do |line|
+      splits = line.split(",")
+
+      #Remove the dublicate entry of name and add the frequency while building the HASH
+      if dict_by_year.has_key?(splits[0].downcase)  
+        dict_by_year[splits[0].downcase] = ( Integer(splits[2]) + (dict_by_year[splits[0].downcase]*total_freq) ) / Float(total_freq)
+      else
+        dict_by_year[splits[0].downcase] = Integer(splits[2])/Float(total_freq)
+      end
+    end
+    return dict_by_year.flatten
+  end
+
 end
 
-spellcheck = SpellCheck.new("bhanu", 1990)
+spellcheck = SpellCheck.new("bhanu")
+spellcheck.add_to_dictionary("tes1990.txt")
+spellcheck.add_to_dictionary("tes1920.txt")
+
 
 # spellcheck.add_hash_to_dictionary(:apple => "fruit", :mango => "fruit", :cauliflower => "vegitable")
-
-
-# p spellcheck.test
